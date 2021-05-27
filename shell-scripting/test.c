c93a346a29f27431b03ed57728598eded3b7dd5c
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-05-27 16:44:49 -0500

    update

--- shell-scripting/test.c
+++ shell-scripting/test.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 int main(void){
+    
+    // Call shell script
     // system("C:/Users/nmark/Documents/GitHub/hello-world/my_script.sh");
     system("sine.py");  // calls the python script
 
