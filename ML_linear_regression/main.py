0fb46b4ac3ea3e14abe56619340bebc4514299fc
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-06-16 11:48:42 -0500

    pdate

--- ML_linear_regression/main.py
+++ ML_linear_regression/main.py
@@ -2,17 +2,33 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
+from matplotlib import pyplot as plt
+import pickle
+
 
 dataPath = r"C:\Users\nmark\Documents\GitHub\hello-world\ML_linear_regression\data\student-mat.csv"
 data = pd.read_csv(dataPath,sep=';')
-
-print(data.head())
 
 data = data[["G1","G2","G3","studytime","failures","absences"]]
 
 predict = "G3" # Predict final grade
 
 X = np.array(data.drop(["G3"],1))   # New data frame without G3
-Y = np.array(data[predict]) # Just the G3 value
+Y = np.array(data[predict])         # Just the G3 value
 
 
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X,Y,test_size = 0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train,y_train)
+acc = linear.score(x_test,y_test)
+print(acc)
+
+print("Co: ", linear.coef_)
+print("Intercept: \n", linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for i in range(len(predictions)):
+    print(predictions[i],x_test[i],y_test[i])
