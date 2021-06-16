aa1a490e6230de7dbefa10e27b47a1c173a7bf76
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-06-16 12:08:16 -0500

    move to data folder, create pickle

--- ML_linear_regression/main.py
+++ ML_linear_regression/main.py
@@ -5,7 +5,7 @@ from sklearn import linear_model
 from matplotlib import pyplot as plt
 import pickle
 
-dataPath = r"C:\Users\nmark\Documents\GitHub\hello-world\ML_linear_regression\data\student-mat.csv"
+dataPath = r"data\student-mat.csv"
 data = pd.read_csv(dataPath,sep=';')
 data = data[["G1","G2","G3","studytime","failures","absences"]]
 predict = "G3" # Predict final grade
