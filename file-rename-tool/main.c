0e7cb332d9c93ea8630198ebf9dadc989911bc3c
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-06-16 11:13:51 -0500

    update

--- file-rename-tool/main.c
+++ file-rename-tool/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX_LINE_LENGTH 5
 
 int main(void){
+    FILE *fd;
+    char buff[255];
 
+    fd = fopen("test.txt", "r");
+    int i, n;
+    char buffer[MAX_LINE_LENGTH];
+    /* Read the first line */
+    fgets(buffer, sizeof(buffer), fd);
+    /* That should contain a single integer. */
+    n = atoi(buffer);
+    for (i = 0; i < n; i++){
+        /* Read next line */
+        fgets(buffer, sizeof(buffer), fd);
+        /* Process line here.
+       Note that fgets stores also the line terminator (CR-LF or LF). */
+       printf("Hello %d\r\n",n);
+    }
 
     return 0;
 }
