59b4d0a220b0a44dfe9fff006c3feba146690757
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-06-16 20:36:52 -0500

    move into different files

--- ML/scratch_linear_regression/main.py
+++ ML/scratch_linear_regression/main.py
@@ -14,7 +14,3 @@ num_iterations = 1000 # note our data only has 100 data points
 print('Starting gradient descent at b = {0},m = {1}, error = {2}'.format(initial_b,initial_m,compute_error(initial_b,initial_m,points)))
 (b, m) = gradient_descent_runner(points,initial_b,initial_m,learningRate,num_iterations)
 print('After {0} iterations b = {1}, m = {2}, error = {3}'.format(num_iterations,b,m,compute_error(b,m,points)))
-
-
-
-    
