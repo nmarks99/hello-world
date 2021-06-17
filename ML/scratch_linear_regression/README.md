096ff08e63e1f4f0a220962d4317a6e3781aca05
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-06-16 20:12:31 -0500

    update readme

--- ML/scratch_linear_regression/README.md
+++ ML/scratch_linear_regression/README.md
@@ -3,3 +3,13 @@
 $$
 Error_{(m,b)} = \frac{1}{N} \sum_{i=1}^{N} (y_i - (mx_i + b))^2
 $$
+
+## Partial derivatives of b and m for gradient descent 
+
+$$
+\frac{\partial}{\partial m} = \frac{2}{N} \sum_{i=1}^{N} - x_i(y_i - (mx_i+b))
+$$
+
+$$
+\frac{\partial}{\partial b} = \frac{2}{N} \sum_{i=1}^{N} -(y_i - (mx_i+b))
+$$
--- ML/scratch_linear_regression/main.py
+++ ML/scratch_linear_regression/main.py
@@ -9,7 +9,7 @@ def compute_error(b, m, points):
         # Get the x value
         x = points[i,0]
         # Get the y value
-        y = points[0,i]
+        y = points[i,1]
 
         # Get difference, square it, add it to total error
         totalError += (y - (m*x + b))**2
@@ -19,8 +19,29 @@ def compute_error(b, m, points):
 
 
 
-def gradient_descent_runner():
-    pass
+def gradient_descent_runner(points,starting_b,starting_m,learning_rate,num_iterations):
+    # Starting b and m values
+    b = starting_b
+    m = starting_m
+
+    # Gradient descent
+    for i in range(num_iterations):
+        # update b and m with new more accurate b and m by performing this gradient step
+        (b, m) = step_gradient(b, m, np.array(points),learning_rate)
+    
+    return (b,m)
+
+
+def step_gradient(b_current,m_current,points,learning_rate):
+    
+    b_gradient = 0
+    m_gradient = 0
+
+    for i in range():
+        x = points[i,0]
+        y = points[i,1]
+        # Direction with respect to b and m
+        # Computing partial derivatives of our error function
 
 def run():
     
@@ -36,9 +57,9 @@ def run():
 
     # Step 3 - Train the model
     print('Starting gradient descent at b = {0},m = {1}, error = {2}'.format(initial_b,initial_m,compute_error(initial_b,initial_m)))
-    [b, m] = gradient_descent_runner(points,initial_b,initial_m,learning_rate,num_iterations)
+    (b, m) = gradient_descent_runner(points,initial_b,initial_m,learning_rate,num_iterations)
     print('Ending point at b = {1}, m = {2}, error = {3}'.format(num_iterations,b,compute_error(b,m,points)))
 
-
+# Run the program
 if __name__ == '__main__':
     run()
