3899c57ca3d1c87e70b11fd72c474f1d54a0f439
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-11-24 14:21:24 -0600

    upd openremote.py

--- scripts/openremote.py
+++ scripts/openremote.py
@@ -11,6 +11,20 @@ def isRepo():
     except:
         return False
 
+def isGitHubDir():
+    cwd_name = os.path.basename(os.getcwd())
+    if cwd_name == "GitHub":
+       return True
+    else:
+        return False
+
+if isGitHubDir():
+    url = "https://github.com/"
+    cmd = "explorer " + url
+    os.system(cmd)
+    exit()
+     
+
 if isRepo():
 
     cmd1 = "git config --get remote.origin.url"
@@ -19,3 +33,4 @@ if isRepo():
 
     cmd2 = "explorer " + result
     os.system(cmd2)
+
