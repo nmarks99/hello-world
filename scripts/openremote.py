f93ae428d97da8eb182f5c3314a609af96968187
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-12-10 22:28:51 -0600

    update openremote.py

--- scripts/openremote.py
+++ scripts/openremote.py
@@ -4,6 +4,10 @@ import subprocess
 
 
 def isRepo():
+    '''
+    Check if the current directory is a git repo. If it is return true, otherwise false
+    '''
+
     c = 'git rev-parse --is-inside-work-tree'
     try:
         subprocess.check_output(c, shell=True).decode("utf8")
@@ -12,21 +16,34 @@ def isRepo():
         return False
 
 def isGitHubDir():
+    '''
+    Check if the current directory is called GitHub. Assume that 
+    this is the directory where all your repos are stored. Return 
+    true or false accordingly. 
+    '''
+
     cwd_name = os.path.basename(os.getcwd())
     if cwd_name == "GitHub":
        return True
     else:
         return False
 
-if isGitHubDir():
+# -------------------------------------
+
+if isGitHubDir() is True:
+    '''
+    If the current directory is called GitHub, just open github.com
+    '''
     url = "https://github.com/"
     cmd = "explorer " + url
     os.system(cmd)
     exit()
      
 
-if isRepo():
-
+elif isRepo() is True:
+    '''
+    Open the repository's page. Get the url with the git config command below.
+    '''
     cmd1 = "git config --get remote.origin.url"
     result = subprocess.check_output(cmd1, shell=True)
     result = result.decode('utf8').replace(".git","")
