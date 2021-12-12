38f926859c21a15cefac9ff9e0253af64ab631b2
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-12-12 16:27:12 -0600

    save

--- scripts/rust_openremote/src/main.rs
+++ scripts/rust_openremote/src/main.rs
@@ -2,26 +2,48 @@ use std::process::{Command};
 
 fn main() {
 
-    
-    // If on windows, run the command with cmd 
-    let output = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-                .args(["/C", "git config --get remote.origin.url"])
-                .output()
-                .expect("Command 'git config --get remote.origin.url' failed to execute")
+    // Define a String to store cmd output 
+    let cmd_output: String;
+
+    // Check the operating system
+    let windows = cfg!(target_os = "windows");
+
+
+    if windows {
+        cmd_output = v_windows();
+    }
+    else {
+        cmd_output = v_unix();
     }
 
-    // run throught sh if Mac or Linux
-    else{
-        Command::new("sh")
-                .arg("-c")
+    let repo_link:&str = &cmd_output[0..cmd_output.len()-5]; // remove the .git at the end
+    open::that(repo_link).expect("Couldn't open the repo in browser");
+
+}
+
+// fn isRepo(){
+//     let check_if_repo_cmd = "git rev-parse --is-inside-work-tree";
+//     Command::new("cmd")
+// }
+
+fn v_windows() -> String {
+    let output = Command::new("cmd")
+                .arg("/C") // this is important but idk why
                 .arg("git config --get remote.origin.url")
                 .output()
-                .expect("Command 'git config --get remote.origin.url' failed to execute")
-    };
+                .expect("Command 'git config --get remote.origin.url' failed to execute");
 
+    let output = String::from_utf8(output.stdout).unwrap();
+    return output;
+}
 
-    let github_repo_path = String::from_utf8(output.stdout).unwrap();
-    open::that(github_repo_path).expect("Couldn't open the repo in browser");
+fn v_unix() -> String {
+    let output = Command::new("sh")
+                .arg("-c") // this is important but idk why
+                .arg("git config --get remote.origin.url")
+                .output()
+                .expect("Command 'git config --get remote.origin.url' failed to execute");
 
+    let output = String::from_utf8(output.stdout).to_owned().unwrap();
+    return output
 }
