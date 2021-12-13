07a410241e6ce7ba23b191c33ef523e1bd6e6b90
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-12-12 20:29:49 -0600

    made sys_command function and added a helpers.rs file as a module. Rust is nice :)

--- scripts/rust_openremote/src/main.rs
+++ scripts/rust_openremote/src/main.rs
@@ -1,23 +1,16 @@
-use std::process::{Command};
+mod helpers;
 
 fn main() {
 
-    // Define a String to store cmd output 
-    let cmd_output: String;
+    let cmd_output:String;
+    let get_repo_url_cmd = "git config --get remote.origin.url".to_string();
 
-    // Check the operating system
-    let windows = cfg!(target_os = "windows");
-
-
-    if windows {
-        cmd_output = v_windows();
-    }
-    else {
-        cmd_output = v_unix();
-    }
-
+    // Run the command
+    cmd_output = helpers::sys_command(get_repo_url_cmd);
     let repo_link:&str = &cmd_output[0..cmd_output.len()-5]; // remove the .git at the end
-    open::that(repo_link).expect("Couldn't open the repo in browser");
+    
+    // Open url in browser
+    open::that(repo_link).expect("Couldn't open the repo in browser"); 
 
 }
 
@@ -25,25 +18,3 @@ fn main() {
 //     let check_if_repo_cmd = "git rev-parse --is-inside-work-tree";
 //     Command::new("cmd")
 // }
-
-fn v_windows() -> String {
-    let output = Command::new("cmd")
-                .arg("/C") // this is important but idk why
-                .arg("git config --get remote.origin.url")
-                .output()
-                .expect("Command 'git config --get remote.origin.url' failed to execute");
-
-    let output = String::from_utf8(output.stdout).unwrap();
-    return output;
-}
-
-fn v_unix() -> String {
-    let output = Command::new("sh")
-                .arg("-c") // this is important but idk why
-                .arg("git config --get remote.origin.url")
-                .output()
-                .expect("Command 'git config --get remote.origin.url' failed to execute");
-
-    let output = String::from_utf8(output.stdout).to_owned().unwrap();
-    return output
-}
---
+++ scripts/rust_openremote/tests/testing.rs
@@ -0,0 +1,35 @@
+use std::process::{Command};
+
+fn main() {
+    let command = "ls".to_string();
+    let res = sys_command(command);
+    println!("{}",res);
+}
+
+fn sys_command(command:String) -> String {
+
+    // See if on windows 
+    let windows = cfg!(target_os = "windows");
+    let prog:&str;
+    let prog_flag:&str;
+
+    if windows == true {
+        prog = "cmd";
+        prog_flag = "/C";
+    }
+    else{
+        // Presumably the other option is a UNIX machine with "sh"
+        // I think this is a reasonable assumption
+        prog = "sh";
+        prog_flag = "-c";
+    }
+
+    let output = Command::new(prog)
+                .arg(prog_flag) // this is important but idk why
+                .arg(command)
+                .output()
+                .expect("Command failed to execute");
+
+    let output = String::from_utf8(output.stdout).unwrap();
+    return output;
+}
