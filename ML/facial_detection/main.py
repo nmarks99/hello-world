fa2cc493daebcbe688bcb292b265beb58f01e558
Nick <nmarks99@gmail.com>
2022-01-24 16:07:41 -0600

    compute elapsed time

--- ML/facial_detection/main.py
+++ ML/facial_detection/main.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 import cv2
 import sys
+import time
 
 assert(len(sys.argv) == 2),'No input given. Please input path to image'
 img_path = str(sys.argv[1])
 
+t0 = time.time()
+
 # Load the cascade
+print('Analyzing image...')
 face_cascade = cv2.CascadeClassifier('face_detector.xml')
 
 # Read the image
 img = cv2.imread(img_path)
-
-print('Running...')
 
 # Detect faces
 faces = face_cascade.detectMultiScale(img, 1.1, 4)
@@ -22,5 +24,10 @@ for (x,y,w,h) in faces:
 
 # Save the result 
 cv2.imwrite('out.png',img)
+tf = time.time()
+elapsed = tf-t0
 print('\nDone. Result saved to out.png')
+print('Elapsed time = {:.4f} s'.format(elapsed))
+
+
 
