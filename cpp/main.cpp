51832536ab03d899e2a6762064bace56fcd8e8ca
Nick <nmarks99@gmail.com>
2022-02-14 00:25:27 -0500

    split_string works!

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,30 +1,43 @@
-#include "mystrings.h"
+#include <iostream>
+#include <string>
+#include <vector>
 
+void split_string(std::string instring, std::vector<std::string> &split_vec, char delimiter);
+
+using namespace std;
 int main() {
 
+    string myString = "one,two,three,four";
 
-    std::string myString = "one,two,three";
-
-    std::cout << myString[5];
-    std::cout << "\n";
-
-    // char delim = ',';
-    // std::vector<char> split_arr;
-    // split_string(myString,delim,split_arr);
-    // int i = 0;
-    // for (i; i < split_arr.size(); i++){
-    //     std::cout << split_arr[i];
-    //     std::cout << "\n";
-    // }
-
-        
-    // std::string x;
-
-    // // read user input
-    // std::cin >> x;
-
-    // std::string text = "You said ";
-    // std::cout << text.append(x).append("\n");
+    char delim = ',';
+    vector<string> split_vec;
+    split_string(myString, split_vec, delim);
+    
+    for (int i=0; i < split_vec.size(); i++){
+        cout <<  split_vec[i] << "\n";
+    }
 
     return 0;
 }
+
+void split_string(std::string instring, std::vector<std::string> &split_vec, char delimiter){
+    unsigned const int LEN = instring.length();
+    unsigned int i = 0;
+    std::vector<char> temp_vec;
+
+    for (i; i <= LEN; i++){    
+        if (instring[i] != delimiter){
+            temp_vec.push_back(instring[i]);
+        }
+        else{
+            // convert char vec to string and append it to split_vecay
+            split_vec.push_back(std::string(temp_vec.begin(), temp_vec.end()));
+            temp_vec.clear(); // clear temp vec 
+        }
+        
+        if (i == LEN) {
+            split_vec.push_back(std::string(temp_vec.begin(), temp_vec.end()));
+            temp_vec.clear(); // clear temp vec 
+        }
+    }
+}   
