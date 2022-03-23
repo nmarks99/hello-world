048b2e4c96d8f1a5f22ff049fc0be1e1658dbf17
Nick <nmarks99@gmail.com>
2022-03-23 15:11:54 -0500

    update

--- cpp/tui/ftxui-starter/src/main.cpp
+++ cpp/tui/ftxui-starter/src/main.cpp
@@ -1,43 +1,4 @@
-// #include <iostream>
-//
-// #include "ftxui/dom/elements.hpp"
-// #include "ftxui/screen/screen.hpp"
-// #include "ftxui/screen/string.hpp"
-//
-// int main(void) {
-  // using namespace ftxui;
-//
-  // auto summary = [&] {
-    // auto content = vbox({
-        // hbox({text(L"- hello:   "), text(L"3") | bold}) | color(Color::Green),
-        // hbox({text(L"- goodbye: "), text(L"2") | bold}) | color(Color::RedLight),
-        // hbox({text(L"- Hello again:  "), text(L"9") | bold}) | color(Color::Red),
-    // });
-    // return window(text(L" Summary "), content);
-  // };
-//
-  // auto document =  //
-      // vbox({
-          // hbox({
-              // summary(),
-              // summary(),
-              // summary() | flex,
-          // }),
-          // summary(),
-          // summary(),
-      // });
-//
-  // // Limit the size of the document to 80 char.
-  // document = document | size(WIDTH, LESS_THAN, 80);
-//
-  // auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
-  // Render(screen, document);
-//
-  // std::cout << screen.ToString() << std::endl;
-//
-  // return EXIT_SUCCESS;
-// }
-//
+
 #include <chrono>                  // for operator""s, chrono_literals
 #include <cmath>                   // for sin
 #include <ftxui/dom/elements.hpp>  // for operator|, graph, separator, color, Element, vbox, flex, inverted, Fit, hbox, size, border, GREATER_THAN, HEIGHT
@@ -90,16 +51,16 @@ int main(int argc, const char* argv[]) {
             vbox({
                 graph(std::ref(my_graph)),
                 separator(),
-                graph(triangle) | inverted,
+                // graph(triangle) | inverted,
             }) | flex,
-            separator(),
-            vbox({
-                graph(std::ref(my_graph)) | color(Color::BlueLight),
-                separator(),
-                graph(std::ref(my_graph)) | color(Color::RedLight),
-                separator(),
-                graph(std::ref(my_graph)) | color(Color::YellowLight),
-            }) | flex,
+            // separator(),
+            // vbox({
+                // graph(std::ref(my_graph)) | color(Color::BlueLight),
+                // separator(),
+                // graph(std::ref(my_graph)) | color(Color::RedLight),
+                // separator(),
+                // graph(std::ref(my_graph)) | color(Color::YellowLight),
+            // }) | flex,
         }) |
         border | size(HEIGHT, GREATER_THAN, 40);
 
