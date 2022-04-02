31cbf1221bbc9356053dce0516ffac85f08c18fa
Nick <nmarks99@gmail.com>
2022-04-01 23:16:15 -0500

    add tui

---
+++ C/tui/main.c
@@ -0,0 +1,9 @@
+#include "tui.h"
+
+int main(void) {
+
+    printf("%shello world%s\n",ANSI_MAGENTA, ANSI_RESET);
+    printf("%shello world%s\n",ANSI_BOLD_MAGENTA, ANSI_RESET);
+
+}
+
---
+++ C/tui/tui.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* Define ANSI escape codes */
+
+#define ANSI_RESET  "\x1B[0m"
+#define ANSI_RED  "\x1B[0;31m"
+#define ANSI_GREEN  "\x1B[0;32m"
+#define ANSI_YELLLOW  "\x1B[0;33m"
+#define ANSI_BLUE  "\x1B[0;34m"
+#define ANSI_MAGENTA  "\x1B[0;35m"
+#define ANSI_CYAN  "\x1B[0;36m"
+#define ANSI_WHITE  "\x1B[0;37m"
+
+#define ANSI_BOLD_RED  "\x1B[1;31m"
+#define ANSI_BOLD_GREEN  "\x1B[1;32m"
+#define ANSI_BOLD_YELLLOW  "\x1B[1;33m"
+#define ANSI_BOLD_BLUU  "\x1B[1;34m"
+#define ANSI_BOLD_MAGENTA  "\x1B[1;35m"
+#define ANSI_BOLD_CYAN  "\x1B[1;36m"
+#define ANSI_BOLD_WHITE  "\x1B[1;37m"
+
