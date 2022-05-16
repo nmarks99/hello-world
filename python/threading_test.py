1b0d33820e3f4701f2f414019b84f2c03a345c74
Nick <nmarks99@gmail.com>
2022-05-16 17:10:24 -0500

    save

--- python/threading_test.py
+++ python/threading_test.py
@@ -1,21 +1,28 @@
 import threading
-import time 
-import os
+import time
+from dataclasses import dataclass
+
+def poll():
+    ax = 1
+    ay = 2
+    az = 3
+    amag = 4
+    return ax, ay, az, amag
+
 
 def func(arr):
     t0 = time.time()
     while True:
+
         elap = time.time() - t0
-        
-        arr.append(elap)
+        ax, ay, az, amag = poll()
+        arr.append([ax, ay, az, amag, elap])
         time.sleep(0.1)
         global stop_thread
         if stop_thread:
             break
 
-
 # Define thread
-
 stop_thread = False
 arr = []
 x = threading.Thread(target=func,args=(arr,))
